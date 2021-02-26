dce34e87fd6cde7a4e43772c280225339e1f4ee7
Darius Dubose <ddubose1@sundevils.asu.edu>
2021-02-25 23:24:56 -0800

    Add project files.

---
+++ Xpressed_N_Ink/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Xpressed_N_Ink.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
